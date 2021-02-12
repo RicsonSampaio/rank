a5d06b147e9465f878a519d14ae24abd13b6a738
RicsonSampaio <ricsonsampaio@gmail.com>
2021-02-11 21:31:29 -0300

    first-action

---
+++ Controllers/TopTenController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rank.Controllers
+{
+    [Route("api/[controller]")]
+    public class TopTenController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetTopTen()
+        {
+            return Ok(new { name = "Kalazan", rank = "3" });
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -12,6 +12,7 @@ namespace Rank
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -23,13 +24,13 @@ namespace Rank
             }
 
             app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
